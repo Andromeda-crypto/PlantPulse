cfb9291d79d45d74245158974f569119d23fe3cf
Andromeda-crypto <145452127+Andromeda-crypto@users.noreply.github.com>
2025-05-25 22:42:32 +0530

    added more ml logic for better analysis

---
+++ ml_engine.py
@@ -0,0 +1,33 @@
+import cv2
+from image_utils import load_resize_image, is_image_blurry, calculate_image_features
+# later: will import ML model loaders 
+
+def analyze_plant_image(filepath):
+    img, err = load_resize_image(filepath)
+    if img is None:
+        return {"status": "error", "message": err}
+    if is_image_blurry(img):
+        return {
+            "status": "blurry",
+            "message": "The image is too blurry for analysis. Try uploading a clearer photo."
+        }
+    
+    features = calculate_image_features(img)
+
+    plant_type = classify_plant(img) # Later
+    health_status = diagnose_health(img,features) # later
+    suggestions = generate_suggestions(health_status,featues) # later 
+
+    return {
+        "status": "ok",
+        "plant_type": plant_type,
+        "health_status": health_status,
+        "suggestions": suggestions,
+        "features": features
+    }
+
+def clssify_palnt(img):
+    # Dummy for now, will be replaced with actual ML model
+    return "Unknown Plant Type"
+
+
