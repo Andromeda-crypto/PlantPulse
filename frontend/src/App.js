aa41733be4f29dc80cef00505bc1e500f2569e22
Andromeda-crypto <145452127+Andromeda-crypto@users.noreply.github.com>
2025-06-02 07:15:31 +0530

    adding routes to react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ZoomForm from './components/ZoomForm';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
+import { AuthProvider } from './context/AuthContext';
+import PrivateRoute from './components/PrivateRoute'; 
+import QueryForm from './components/QueryForm';
+import HomeForm from './components/Home';
+import PhotoForm from './components/Photoform';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Zoom View</h2>
-      <ZoomForm />
+      <h2>Home</h2>
+      <HomeForm />
     </div>
   );
 }
