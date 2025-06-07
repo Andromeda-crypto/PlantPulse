e02c57241787d3ef2476ea7673566cc4eee910b4
Andromeda-crypto <145452127+Andromeda-crypto@users.noreply.github.com>
2025-06-07 18:09:26 +0530

    modfying API calls

--- frontend/src/components/SignupForm.js
+++ frontend/src/components/SignupForm.js
@@ -13,7 +13,7 @@ export default function SignupForm({ onSignupSuccess }) {
     e.preventDefault();
 
     try {
-      const res = await fetch('http://localhost:5000/signup', {
+      const res = await fetch('http://127.0.0.1:3000/templates/signup.html', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
